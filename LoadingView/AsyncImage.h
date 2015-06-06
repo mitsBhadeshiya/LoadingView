673a6a5f8b518865b264bd6afe471160504770e9
mitsBhadeshiya <abc@ABCs-Mac-mini.local>
2015-06-06 22:36:01 +0530

    sds
    asdsada

---
+++ LoadingView/AsyncImage.h
@@ -0,0 +1,47 @@
+//
+//  AsyncImage.h
+//  LenderApp
+//
+//  Created by Mitul on 06/12/14.
+//  Copyright (c) 2014 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@protocol AsyncImageDelegate<NSObject>
+
+@optional
+
+-(void) onAsyncTouch:(id)asyncImg;
+-(void) FetchImageSuccess:(id)asyncImg;
+-(void) FetchImageFail:(id)asyncImg;
+
+@end
+
+@interface AsyncImage : UIImageView<NSURLConnectionDataDelegate,NSURLConnectionDelegate>
+{
+    NSURLConnection* connection;
+	NSMutableData* data;
+	UIActivityIndicatorView *scrollingWheel;
+    NSString *imgName;
+    NSString *imgName2;
+    NSString *strFolderName;
+}
+
+@property (nonatomic, strong) id<AsyncImageDelegate> delegate;
+@property (nonatomic, strong) UIActivityIndicatorView *scrollingWheel;
+@property (nonatomic, strong) NSIndexPath *indexPath;
+
+@property SEL onAsyncTouchSelector;
+
+//- (void)loadImageFromExistingImage:(NSString*)stringUrl Folder:(NSString *)FolderName;
+//-(void)loadImageFromStringforUserimg:(NSString*)url;
+-(void)loadImageFromString:(NSString*)url;
+
+-(void)cancelConnection;
+
+-(id)initWithCoder:(NSCoder *)aDecoder;
+-(id)initWithFrame:(CGRect)frame;
+-(void)addGestureRecognizer;
+@end
